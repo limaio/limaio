ef4a4d9c5fc6c6d7f7df9f5e1527911e431fb25f
Juan Peña <juan3789@gmail.com>
2012-12-02 16:47:12 -0500

    Corrigiendo escalas de sensores de UV y Polvo

--- sites/all/insert.php
+++ sites/all/insert.php
@@ -44,9 +44,11 @@ if($uv < 0){
     $uv = round(($uv)*12/22,2);
 }*/
 
-$uv = $sensor3/10;
-if($sensor6 <= 3500){
-    $dust = round($sensor6*0.00015-0.05,2);
+$uv = round($sensor3*500/1024,2);
+
+$sensor6 = $sensor6*5/1024;
+if($sensor6 <= 3.5){
+    $dust = round($sensor6*0.15-0.05,2);
 }else{
     $dust = 0.5;
 }
